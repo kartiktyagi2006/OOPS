516cafa2d8d01b603520dd41542a869b1f56d2fa
kartiktyagi2006 <kartikatyagi2006@gmail.com>
2026-02-11 08:34:35 +0530

    Add files via upload

---
+++ car.java
@@ -0,0 +1,12 @@
+public class car {
+
+    public static void main(String[] args) {
+
+        int car_modelnum;
+        int car_price;
+        car_modelnum = 101;
+        car_price = 800000;
+        System.out.println("Car Model Number: " + car_modelnum);
+        System.out.println("Car Price: " + car_price);
+    }
+}
